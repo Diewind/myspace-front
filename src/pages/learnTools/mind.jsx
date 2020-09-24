bdd58f3c5fb2bc936ca879e894e0cf8725f9c56f
yanfeng.zhang <sh_fight@163.com>
2020-09-24 17:10:38 +0800

    修改脑图

--- src/pages/learnTools/mind.jsx
+++ src/pages/learnTools/mind.jsx
@@ -7,19 +7,27 @@ import './mind.less';
 import mindDatas from './mindDatas'
 
 class Mind extends Component {
+
     constructor(props){
         super(props);
         this.state = {
-            zoom:0.2,
-            nodeTit:'canvas',
-            nodeName:''
+            nodeTit:'canvas'
         }
     }
+
     componentDidMount() {
+        this.init();
+    }
+
+    /**
+     * 初始化页面
+     */
+    init = () => {
+        
         const content = this.refs.content;
         const tool = this.refs.tool;
-        // const detail = this.refs.detail;
-        // const contextmenu = this.refs.contextmenu;
+        const detail = this.refs.detail;
+        const contextmenu = this.refs.contextmenu;
         const minimap = this.refs.minimap;
         const editor = new G6Editor();
         const {setFieldsValue} = this.props.form;
@@ -29,22 +37,15 @@ class Mind extends Component {
             viewportBackStyle:'#fff',
             viewportWindowStyle:'#fff'
         });
-
-        
-        
         const toolBox = new G6Editor.Toolbar({
             container: tool
         });
-
-        // const contextmenuBox = new G6Editor.Contextmenu({
-        //     container: contextmenu
-        // });
-
-        // const detailBox = new G6Editor.Detailpannel({
-        //     container: detail
-        // });
-        
-
+        const contextmenuBox = new G6Editor.Contextmenu({
+            container: contextmenu
+        });
+        const detailBox = new G6Editor.Detailpannel({
+            container: detail
+        });
         const contentBox = new G6Editor.Mind({
             defaultData: mindDatas,
             graph: {
@@ -55,36 +56,30 @@ class Mind extends Component {
 
         editor.add(toolBox);
         editor.add(minimapBox);
-        // editor.add(contextmenuBox);
-        // editor.add(detailBox);
+        editor.add(contextmenuBox);
+        editor.add(detailBox);
         editor.add(contentBox);
 
         const curPage = editor.getCurrentPage();
         curPage.on('click',(ev)=>{
-            console.log('ev',ev);
+            console.log('click',ev);
             let title = ev?.item?.model.label || '';
             let nodeTit = ev?.item?.type || 'canvas';
-            setFieldsValue({
-                'LabelName':title,
-                'nodeName':title
-            });
             this.setState({
                 nodeTit
-            })
-        })
-        console.log('page',curPage);
-
-
-        this.setState({
-            zoom:0.2,
-            editor
-        })
-        // mind.showHotArea();
+            });
+            setFieldsValue({
+                'labelName':title
+            });
+        });
     }
 
-    onRoom = () => {
-        const {editor} = this.state;
-        editor.executeCommand('zoomOut');
+    handleBlur = (e) => {
+        console.log('eeee2',e,e.value);
+    }
+
+    handleChange = (e) => {
+        console.log('eeee',e);
     }
 
     importFile = () => {
@@ -119,14 +114,14 @@ class Mind extends Component {
                         <div className="toolbar" ref='tool'>
                             <div className="command" data-command="undo">
                                 <Tooltip title='Undo' placement="bottom">
-                                    <span className='toolicon disabled' role='img'>
+                                    <span className='toolicon ' role='img'>
                                         <svg id="icon-undo" viewBox="0 0 1024 1024"><path d="M143.14 449.19q69.07-89.09 170.67-140.64Q415.41 257 537.52 256q183.18 4 315.81 114.11T1024 654.39q-58.06-107.11-161.66-170.17-103.6-63.06-232.73-65.06-107.1 1-196.69 45.54-89.58 44.55-152.65 121.62L407.4 713.45q7 7 7 17.01 0 10.01-7 17.02t-17.01 7.01H32.04q-14.01 0-23.02-9.01T0.01 722.46V364.11q0-10.01 7-17.02t17.02-7.01q10.01 0 17.02 7.01l102.1 102.1z"></path></svg>
                                     </span>
                                 </Tooltip>
                             </div>
                             <div className="command" data-command="redo">
                                 <Tooltip title='Redo' placement="bottom">
-                                    <span className='toolicon disabled' role='img'>
+                                    <span className='toolicon ' role='img'>
                                         <svg id="icon-redo" viewBox="0 0 1024 1024"><path d="M999.98 340.08q-10.01 0-17.02 7.01l-102.1 102.1q-69.07-89.09-170.67-140.64Q608.59 257 487.48 256q-184.18 4-316.81 114.11T0 654.39q58.06-107.11 161.66-170.17 103.6-63.06 232.73-65.06 107.1 1 197.19 45.54 90.09 44.55 152.15 121.62L616.6 713.45q-7 7-7 17.01 0 10.01 7 17.02t17.01 7.01h358.35q14.01 0 23.02-9.01t9.01-23.02V364.11q0-10.01-7-17.02t-17.01-7.01z"></path></svg>
                                     </span>
                                 </Tooltip>
@@ -164,14 +159,14 @@ class Mind extends Component {
                             <div className="ant-divider ant-divider-vertical" role="separator"></div>
                             <div className="command" data-command="append">
                                 <Tooltip title='Topic' placement="bottom">
-                                    <span className='toolicon disabled' role='img'>
+                                    <span className='toolicon ' role='img'>
                                         <svg id="icon-append" viewBox="0 0 1024 1024"><path d="M149.454 116.492h65.915v65.916h-65.915v-65.916z m131.83 0h65.924v65.916h-65.923v-65.916z m-131.83 131.839h65.915v65.915h-65.915v-65.915z m263.669-131.839h65.915v65.916h-65.915v-65.916z m263.67 0h65.922v65.916h-65.923v-65.916z m0 263.67h65.922v65.923h-65.923V380.16z m-131.831-263.67h65.915v65.916h-65.915v-65.916z m-263.677 263.67h65.923v65.923h-65.923V380.16zM808.63 248.33h65.915v65.915h-65.915v-65.915zM149.454 380.16h65.915v65.924h-65.915V380.16zM808.63 116.492h65.915v65.916h-65.915v-65.916z m0 263.67h65.915v65.923h-65.915V380.16z m-395.508 0h65.915v65.923h-65.915V380.16z m131.839 0h65.915v65.923h-65.915V380.16z m0 65.923h-65.924v131.83H149.454v329.593h725.092V577.915H544.962v-131.83zM808.63 841.592H215.369V643.84h593.262v197.753z"></path></svg>
                                     </span>
                                 </Tooltip>
                             </div>
                             <div className="command" data-command="appendChild">
                                 <Tooltip title='Subtopic' placement="bottom">
-                                    <span className='toolicon disabled' role='img'>
+                                    <span className='toolicon ' role='img'>
                                         <svg id="icon-append-child" viewBox="0 0 1024 1024"><path d="M256 128h64v64h-64v-64z m128 0h64v64h-64v-64z m0 256h64v64h-64v-64z m128 0h64v64h-64v-64z m0-256h64v64h-64v-64z m0 128h64v64h-64v-64zM128 128h64v64h-64v-64zM0 128h64v64H0v-64z m0 128h64v64H0v-64z m256 128h64v64h-64v-64z m-128 0h64v64h-64v-64zM0 384h64v64H0v-64z m448 128v128H192V512h-64v192h320v128h576V512H448z m512 256H512V576h448v192z"></path></svg>
                                     </span>
                                 </Tooltip>
@@ -179,14 +174,14 @@ class Mind extends Component {
                             <div className="ant-divider ant-divider-vertical" role="separator"></div>
                             <div className="command" data-command="collapse">
                                 <Tooltip title='Fold' placement="bottom">
-                                    <span className='toolicon disabled' role='img'>
+                                    <span className='toolicon ' role='img'>
                                         <svg id="icon-collapse" viewBox="0 0 1024 1024"><path d="M704 576H576v128H0V384h576v128h128V256h320v64H768v192h256v64H768v192h256v64H704V576zM256 160v96L64 128 256 0v96h346v64H256z"></path></svg>
                                     </span>
                                 </Tooltip>
                             </div>
                             <div className="command" data-command="expand">
                                 <Tooltip title='Unfold' placement="bottom">
-                                    <span className='toolicon disabled' role='img'>
+                                    <span className='toolicon ' role='img'>
                                         <svg id="icon-expand" viewBox="0 0 1024 1024"><path d="M448 160H102V96h346V0l192 128-192 128v-96z m256 416H576v128H0V384h576v128h128V256h320v64H768v192h256v64H768v192h256v64H704V576z"></path></svg>
                                     </span>
                                 </Tooltip>
@@ -196,31 +191,87 @@ class Mind extends Component {
                     <div className="mindbox-body-bd">
                         <div className="mindbox-body-bd-content" ref='content'></div>
                         <div className="mindbox-body-bd-sidebar" ref='sidebar'>
-                            <div className="node-panel">
-                                <Card size='small' title={this.state.nodeTit}>
-                                    {this.state.nodeTit === 'node' && <Form layout='horizontal'>
-                                        <Form.Item label="Label" {...FOEM_ITEM_LAYOUT}>
-                                            {getFieldDecorator('LabelName', {
-                                                initialValue:this.state.nodeName
-                                            })(<Input />)}
-                                        </Form.Item>
-                                    </Form>}
-                                    {/* <div className="contextmenu" ref="contextmenu"></div>
-                                    <div className="detail" ref='detail'>
-                                        <div data-status="node-selected">
-                                        xx属性栏
-                                        </div>
-                                        <div data-status="edge-selected">边属性栏</div>
-                                        <div data-status="group-selected">群组属性栏</div>
-                                        <div data-status="canvas-selected">画布属性栏</div>
-                                        <div data-status="multi-selected">多选时属性栏</div>
-                                    </div> */}
-                                </Card>
-                            </div>
+                            <Card size='small' type="inner" title={this.state.nodeTit}>
+                                {this.state.nodeTit === 'node' && <Form layout='horizontal'>
+                                    <Form.Item label="Label" {...FOEM_ITEM_LAYOUT}>
+                                        {getFieldDecorator('labelName', {
+                                            
+                                        })(<Input onChange={this.handleChange} onBlur={this.handleBlur} />)}
+                                    </Form.Item>
+                                </Form>}
+                                
+                            </Card>
                             
-                            <Card size='small' title='Minimap'>
+                            <Card size='small' type="inner" title='minimap'>
                                 <div className="minimap" ref='minimap'></div>
                             </Card>
+                        </div>
+                    </div>
+                    <div className="mindbox-body-contextmenu" ref="contextmenu">
+                        <div className="mindbox-body-contextmenu-detail" ref='detail'>
+                            <div class="menu" data-status="node-selected">
+                                <div class="command" data-command="append">
+                                    <div class="context-menu-index-item">
+                                        <span role="img" class="anticon">
+                                            <svg id="icon-append" viewBox="0 0 1024 1024"><path d="M149.454 116.492h65.915v65.916h-65.915v-65.916z m131.83 0h65.924v65.916h-65.923v-65.916z m-131.83 131.839h65.915v65.915h-65.915v-65.915z m263.669-131.839h65.915v65.916h-65.915v-65.916z m263.67 0h65.922v65.916h-65.923v-65.916z m0 263.67h65.922v65.923h-65.923V380.16z m-131.831-263.67h65.915v65.916h-65.915v-65.916z m-263.677 263.67h65.923v65.923h-65.923V380.16zM808.63 248.33h65.915v65.915h-65.915v-65.915zM149.454 380.16h65.915v65.924h-65.915V380.16zM808.63 116.492h65.915v65.916h-65.915v-65.916z m0 263.67h65.915v65.923h-65.915V380.16z m-395.508 0h65.915v65.923h-65.915V380.16z m131.839 0h65.915v65.923h-65.915V380.16z m0 65.923h-65.924v131.83H149.454v329.593h725.092V577.915H544.962v-131.83zM808.63 841.592H215.369V643.84h593.262v197.753z"></path></svg>
+                                        </span>
+                                        <span>Topic</span>
+                                    </div>
+                                </div>
+                                <div class="command" data-command="appendChild">
+                                    <div class="context-menu-index-item">
+                                        <span role="img" class="anticon">
+                                            <svg id="icon-append-child" viewBox="0 0 1024 1024"><path d="M256 128h64v64h-64v-64z m128 0h64v64h-64v-64z m0 256h64v64h-64v-64z m128 0h64v64h-64v-64z m0-256h64v64h-64v-64z m0 128h64v64h-64v-64zM128 128h64v64h-64v-64zM0 128h64v64H0v-64z m0 128h64v64H0v-64z m256 128h64v64h-64v-64z m-128 0h64v64h-64v-64zM0 384h64v64H0v-64z m448 128v128H192V512h-64v192h320v128h576V512H448z m512 256H512V576h448v192z"></path></svg>
+                                        </span>
+                                        <span>Subtopic</span>
+                                    </div>
+                                </div>
+                                <div class="command" data-command="collapse">
+                                    <div class="context-menu-index-item">
+                                        <span role="img" class="anticon">
+                                            <svg id="icon-collapse" viewBox="0 0 1024 1024"><path d="M704 576H576v128H0V384h576v128h128V256h320v64H768v192h256v64H768v192h256v64H704V576zM256 160v96L64 128 256 0v96h346v64H256z"></path></svg>
+                                        </span>
+                                        <span>Fold</span>
+                                    </div>
+                                </div>
+                                <div class="command disable" data-command="expand">
+                                    <div class="context-menu-index-item">
+                                        <span role="img" class="anticon">
+                                            <svg id="icon-expand" viewBox="0 0 1024 1024"><path d="M448 160H102V96h346V0l192 128-192 128v-96z m256 416H576v128H0V384h576v128h128V256h320v64H768v192h256v64H768v192h256v64H704V576z"></path></svg>
+                                        </span>
+                                        <span>Unfold</span>
+                                    </div>
+                                </div>
+                                <div class="command" data-command="delete">
+                                    <div class="context-menu-index-item">
+                                        <span role="img" class="anticon">
+                                            <svg id="icon-delete" viewBox="0 0 1024 1024"><path d="M828.55 311.65q-17.81 0-30.05 11.13-12.25 11.13-14.47 28.94l-52.32 529.81h-95.72l16.7-461.91q0-14.47-10.02-25.05-10.02-10.57-25.05-11.13-15.02-0.56-25.6 8.91-10.58 9.46-10.58 25.04l-16.69 464.14H459.01l-15.58-465.25q-1.12-15.58-11.69-25.04-10.57-9.47-25.6-8.91-15.03 0.56-24.49 11.13-9.46 10.57-9.46 25.04l15.58 463.03h-94.6l-52.32-529.81q-2.22-17.81-14.47-28.94-12.24-11.13-30.05-11.13h-3.34q-15.58 2.23-25.04 13.36-9.46 11.13-8.35 26.71l60.1 599.93q3.34 31.17 25.6 51.21 22.26 20.03 53.43 21.14h426.3q31.16-1.11 53.42-21.14t26.72-51.21l58.99-603.27q0-15.58-10.57-26.15-10.58-10.58-25.05-10.58z m107.96-71.23l-7.79-61.22q-5.56-35.62-31.72-57.88-26.15-22.26-61.77-23.37h-170.3l-3.34-32.28q-2.22-28.94-22.25-46.75Q619.3 1.11 590.36 0H433.42Q404.48 1.11 385 18.92q-19.48 17.81-22.82 46.75l-2.22 32.28H188.55q-35.62 1.11-61.22 23.37T96.17 179.2l-8.91 63.44q0 7.79 5.01 12.8 5.01 5.01 12.8 5.01h816.98q6.67-1.11 11.13-6.68 4.46-5.56 3.34-13.35zM422.29 97.95l2.22-27.83q1.12-6.68 8.91-7.79h158.05q6.68 1.11 8.91 7.79l1.11 27.83h-179.2z"></path></svg>
+                                        </span>
+                                        <span>Delete</span>
+                                    </div>
+                                </div>
+                            </div>
+                            {/* <div data-status="edge-selected">边属性栏</div>
+                            <div data-status="group-selected">群组属性栏</div> */}
+                            <div class="menu" data-status="canvas-selected">
+                                <div class="command disable" data-command="undo">
+                                    <div class="context-menu-index-item">
+                                        <span role="img" class="anticon">
+                                            <svg id="icon-undo" viewBox="0 0 1024 1024"><path d="M143.14 449.19q69.07-89.09 170.67-140.64Q415.41 257 537.52 256q183.18 4 315.81 114.11T1024 654.39q-58.06-107.11-161.66-170.17-103.6-63.06-232.73-65.06-107.1 1-196.69 45.54-89.58 44.55-152.65 121.62L407.4 713.45q7 7 7 17.01 0 10.01-7 17.02t-17.01 7.01H32.04q-14.01 0-23.02-9.01T0.01 722.46V364.11q0-10.01 7-17.02t17.02-7.01q10.01 0 17.02 7.01l102.1 102.1z"></path></svg>
+                                        </span>
+                                        <span>Undo</span>
+                                    </div>
+                                </div>
+                                <div class="command disable" data-command="redo">
+                                    <div class="context-menu-index-item">
+                                        <span role="img" class="anticon">
+                                            <svg id="icon-redo" viewBox="0 0 1024 1024"><path d="M999.98 340.08q-10.01 0-17.02 7.01l-102.1 102.1q-69.07-89.09-170.67-140.64Q608.59 257 487.48 256q-184.18 4-316.81 114.11T0 654.39q58.06-107.11 161.66-170.17 103.6-63.06 232.73-65.06 107.1 1 197.19 45.54 90.09 44.55 152.15 121.62L616.6 713.45q-7 7-7 17.01 0 10.01 7 17.02t17.01 7.01h358.35q14.01 0 23.02-9.01t9.01-23.02V364.11q0-10.01-7-17.02t-17.01-7.01z"></path></svg>
+                                        </span>
+                                        <span>Redo</span>
+                                    </div>
+                                </div>
+                            </div>
+                            {/* <div data-status="multi-selected">多选时属性栏</div> */}
                         </div>
                     </div>
                 </div>
